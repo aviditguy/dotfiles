19b2eeecf735c2d61b821adb6e7767f65fd5386f
aviditguy <shivamjuyal1999@gmail.com>
2025-11-27 19:55:16 +0530

    first commit

---
+++ bootstrap.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+set -e
+
+DOTFILES_DIR="$HOME/Workspace/dotfiles"
+
+backup_file() {
+    local target="$1"
+    if [ -e "$target" ] && [ ! -L "$target" ]; then
+        local backup="${target}.backup.$(date +%Y%m%d%H%M%S)"
+        echo "Backing up $target -> $backup"
+        mv "$target" "$backup"
+    fi
+}
+
+link_dir() {
+    local src="$1"
+    local dest="$2"
+
+    echo "Linking $src -> $dest"
+
+    backup_file "$dest"
+
+    mkdir -p "$(dirname "$dest")"
+    rm -rf "$dest"
+    ln -s "$src" "$dest"
+}
+
+echo "Using DOTFILES_DIR = $DOTFILES_DIR"
+
+### Emacs
+# dotfiles/emacs -> ~/.config/emacs
+if [ -d "$DOTFILES_DIR/emacs" ]; then
+    link_dir "$DOTFILES_DIR/emacs" "$HOME/.config/emacs"
+fi
+
+### Neovim
+# dotfiles/nvim -> ~/.config/nvim
+if [ -d "$DOTFILES_DIR/nvim" ]; then
+    link_dir "$DOTFILES_DIR/nvim" "$HOME/.config/nvim"
+fi
+
+### i3
+# dotfiles/i3 -> ~/.config/i3
+if [ -d "$DOTFILES_DIR/i3" ]; then
+    link_dir "$DOTFILES_DIR/i3" "$HOME/.config/i3"
+fi
+
+echo "All done!"
+
---
+++ emacs.d/lisp/core.el
@@ -0,0 +1,27 @@
+(setq inhibit-startup-message t)
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(setq ring-bell-function 'ignore)
+(global-display-line-numbers-mode t)
+(setq display-line-numbers-type 'relative)
+(ido-mode 1)
+(ido-everywhere 1)
+(show-paren-mode 1)
+(global-auto-revert-mode 1)
+
+(save-place-mode 1)  ;; reopen file at last cursor position
+
+(recentf-mode 1)
+(add-hook 'emacs-startup-hook #'recentf-open-files)
+
+(add-hook 'org-mode-hook #'org-display-inline-images)  ;; display inline-images org
+
+(setq auto-save-default nil)   ;; Disable auto-saving
+(setq make-backup-files nil)   ;; Disable backup~ files
+(setq create-lockfiles nil)    ;; Disable .#lock files
+
+(setq gc-cons-threshold (* 50 1000 1000))
+(add-hook 'emacs-startup-hook (lambda () (setq gc-cons-threshold (* 2 1000 1000))))
+
+(set-face-attribute 'default nil :font "SauceCodePro NF ExtraLight" :height 140)
