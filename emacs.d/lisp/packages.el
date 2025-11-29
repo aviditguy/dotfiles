ba0e3da536b9dbae614a2c7a7a1d7162290f76e7
aviditguy <shivamjuyal1999@gmail.com>
2025-11-29 18:21:24 +0530

    emacs -- packages.el --> added company + eglot

--- emacs.d/lisp/packages.el
+++ emacs.d/lisp/packages.el
@@ -21,4 +21,29 @@
 ;; Doom themes
 (use-package doom-themes)
 
+
+(use-package company
+  :hook (after-init . global-company-mode)
+  :config
+  (setq company-minimum-prefix-length 1
+        company-idle-delay 0.0))  ;; popup instantly (or set 0.2 if too aggressive)
+
+(use-package eglot
+  :ensure t
+  :hook (python-mode . eglot-ensure)
+  :config
+  (add-to-list 'eglot-server-programs
+               '(python-mode . ("pyright-langserver" "--stdio")))
+  ;; Handy keybindings
+  (define-key eglot-mode-map (kbd "C-c r") #'eglot-rename)
+  (define-key eglot-mode-map (kbd "C-c a") #'eglot-code-actions))
+
+;; Eldoc-box: automatic hover docs
+(use-package eldoc-box
+  :ensure t
+  :after eglot
+  :config
+  (define-key eglot-mode-map (kbd "C-c h") #'eldoc-box-help-at-point))
+
+
 (provide 'packages)
