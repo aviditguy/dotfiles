aa8dc7966b121b61dd8f790424edee5353a4a782
aviditguy <shivamjuyal1999@gmail.com>
2025-11-29 11:30:33 +0530

    emacs added org-config.el

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -3,6 +3,26 @@
 
 (require 'core)
 (require 'packages)
+(require 'org-config)
 (require 'theme)
 (require 'functions)
 (require 'keybindings)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-level-1 ((t (:inherit default :weight normal :height 1.5))))
+ '(org-level-2 ((t (:inherit default :height 1.4))))
+ '(org-level-3 ((t (:inherit default :height 1.3))))
+ '(org-level-4 ((t (:inherit default :height 1.2))))
+ '(org-level-5 ((t (:inherit default :height 1.1))))
+ '(org-level-6 ((t (:inherit default :height 1.1))))
+ '(org-level-7 ((t (:inherit default :height 1.1))))
+ '(org-level-8 ((t (:inherit default :height 1.1)))))
