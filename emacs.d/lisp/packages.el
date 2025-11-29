738361983b40e21c3a6fcdd612f44263d3711dd9
aviditguy <shivamjuyal1999@gmail.com>
2025-11-29 19:24:24 +0530

    emacs updated packages.el

--- emacs.d/lisp/packages.el
+++ emacs.d/lisp/packages.el
@@ -26,14 +26,18 @@
   :hook (after-init . global-company-mode)
   :config
   (setq company-minimum-prefix-length 1
-        company-idle-delay 0.0))  ;; popup instantly (or set 0.2 if too aggressive)
+        company-idle-delay 0.0)
+    (add-to-list 'company-backends 'company-capf))
 
 (use-package eglot
   :ensure t
-  :hook (python-mode . eglot-ensure)
+  :hook ((python-mode . eglot-ensure)
+	 (c-mode      . eglot-ensure)
+	 (c++-mode    . eglot-ensure))
   :config
-  (add-to-list 'eglot-server-programs
-               '(python-mode . ("pyright-langserver" "--stdio")))
+  (add-to-list 'eglot-server-programs '(python-mode . ("pyright-langserver" "--stdio")))
+  (add-to-list 'eglot-server-programs '(c-mode      . ("clangd")))
+  (add-to-list 'eglot-server-programs '(c++-mode    . ("clangd")))
   ;; Handy keybindings
   (define-key eglot-mode-map (kbd "C-c r") #'eglot-rename)
   (define-key eglot-mode-map (kbd "C-c a") #'eglot-code-actions))
@@ -44,6 +48,18 @@
   :after eglot
   :config
   (define-key eglot-mode-map (kbd "C-c h") #'eldoc-box-help-at-point))
+
+(use-package sly
+  :ensure t
+  :config
+  (setq inferior-lisp-program "sbcl")   ;; start SBCL as backend
+  ;; company integration
+  (add-hook 'sly-mode-hook #'company-mode)
+  (add-hook 'sly-mrepl-mode-hook #'company-mode))
+
+(with-eval-after-load 'sly
+  (define-key sly-mode-map (kbd "C-c C-k") #'sly-compile-and-load-file) ; whole file
+  (define-key sly-mode-map (kbd "C-c C-l") #'sly-eval-defun))          ; current defun
 
 
 (provide 'packages)
