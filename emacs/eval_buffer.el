2e1c65cc621c1b9f3ca5aaa520a2c46de992bf0a
aviditguy <shivamjuyal1999@gmail.com>
2026-09-14 19:53:43 +0530

    created evaluate buffer for c/c++/js...

--- emacs/eval_buffer.el
+++ emacs/eval_buffer.el
@@ -1,148 +1,88 @@
 ;; -*- lexical-binding: t; -*-
 
 (defvar my--build-commands
-  '(("py"  . "python %s")
-    ("js"  . "node %s")
-    ("sh"  . "bash %s")
-    ("awk" . "awk -f %s")))
+  '(("py"  . "python %s %s")
+    ("js"  . "node %s %s")
+    ("sh"  . "bash %s %s")
+    ("awk" . "awk -f %s %s")))
 
 
-;; (defvar my--build-command-modes
-;;   '((cc-mode     c-mode-map c++-mode-map)
-;;     (python      python-mode-map)
-;;     (js          js-mode-map)
-;;     (sh-script   sh-mode-map)
-;;     (awk-mode    awk-mode-map)))
+(defun my-parse-header (key &optional file)
+  (let ((inp (or file (buffer-file-name))))
+
+    (with-temp-buffer
+      (insert-file-contents inp nil 0 300)
+      (goto-char (point-min))
+
+      (when (looking-at "^\\(?://\\|#\\)[ \t]*\\(.*\\)$")
+        (let ((params (match-string 1)))
+          (when (string-match
+                 (format "%s[ \t]+\\([^:]*\\)" key)
+                 params)
+            (string-trim (match-string 1 params))))))))
 
 
 (defun my-eval-buffer (&optional file)
   (interactive)
   
-  (let* ((inp (or file (buffer-file-name))))
+  (let ((inp  (or file (buffer-file-name))))
     (unless inp
       (user-error "Buffer is not a visiting a file"))
 
+    (save-buffer)  ;; save file before run
+    
     (let* ((out   (file-name-sans-extension inp))
 	   (ext   (file-name-extension inp))
-	   (flags nil)
 	   (sinp  (shell-quote-argument inp))
-	   (sout  (shell-quote-argument out)))
-
-      ;; get the flags
-      (with-temp-buffer
-	(insert-file-contents inp nil 0 300)
-	(goto-char (point-min))
-	  
-	(setq flags
-	      (if (looking-at "^//[ \t]*\\(.*\\)$")
-		  (match-string 1)
-		"")))
-      
+	   (sout  (shell-quote-argument out))
+	   (flags (or (my-parse-header :flags) ""))
+	   (args  (or (my-parse-header :args) "")))
+    
       (cond
+       ;; for c, c++
        ((member ext '("c" "cpp"))
 	(let ((cc (if (string= ext "c") "gcc" "g++")))
 	  (my-vterm-send
-	   (format "%s -o %s %s %s && %s"
-		   cc sout sinp flags sout))))
-       
+	   (format "%s -o %s %s %s && %s %s"
+		   cc sout sinp flags sout args))))
+
+       ;; for js, python, sh, awk
        ((assoc ext my--build-commands)
 	(my-vterm-send
-	 (format (cdr (assoc ext my--build-commands)) sinp)))
+	 (format (cdr (assoc ext my--build-commands)) sinp args)))
 
        (t
 	(message "No command for language: %s" ext))))))
 
 
-;; (defun my-setup-build-key ()
-;;   (mapc
-;;    (lambda (entry)
-;;      (let ((feature (car entry))
-;;            (maps    (cdr entry)))
+(with-eval-after-load 'cc-mode
+  (mapc
+   (lambda (mode)
+     (define-key
+      (symbol-value mode)
+      (kbd "C-c C-c")
+      #'my-eval-buffer))
+   '(c-mode-map
+     c++-mode-map)))
 
-;;        (eval-after-load
-;;            feature
-;;          `(progn
-;;             ,@(mapcar
-;;                (lambda (map)
-;;                  `(define-key
-;;                     ,map
-;;                     (kbd "C-c C-c")
-;;                     #'my-eval-buffer))
-;;                maps)))))
-;;    my--build-command-modes))
+(with-eval-after-load 'python
+  (define-key python-mode-map
+    (kbd "C-c C-c")
+    #'my-eval-buffer))
 
-;; (defun my-setup-build-key ()
-;;   (mapc
-;;    (lambda (entry)
-;;      (let ((feature (car entry))
-;;            (maps    (cdr entry)))
+(with-eval-after-load 'js
+  (define-key js-mode-map
+    (kbd "C-c C-c")
+    #'my-eval-buffer))
 
-;;        (with-eval-after-load feature
-;;          (mapc
-;;           (lambda (map)
-;;             (define-key
-;;              (symbol-value map)
-;;              (kbd "C-c C-c")
-;;              #'my-eval-buffer))
-;;           maps))))
-;;    my--build-command-modes))
+(with-eval-after-load 'sh-script
+  (define-key sh-mode-map
+    (kbd "C-c C-c")
+    #'my-eval-buffer))
 
-;; (my-setup-build-key)
-
-
-;; (with-eval-after-load 'cc-mode
-;;   (mapc
-;;    (lambda (mode)
-;;      (define-key
-;;       (symbol-value mode)
-;;       (kbd "C-c C-c")
-;;       #'my-eval-buffer))
-;;    '(c-mode-map
-;;      c++-mode-map)))
-
-;; (with-eval-after-load 'python
-;;   (define-key python-mode-map
-;;     (kbd "C-c C-c")
-;;     #'my-eval-buffer))
-
-;; (with-eval-after-load 'js
-;;   (define-key js-mode-map
-;;     (kbd "C-c C-c")
-;;     #'my-eval-buffer))
-
-;; (with-eval-after-load 'sh-script
-;;   (define-key sh-mode-map
-;;     (kbd "C-c C-c")
-;;     #'my-eval-buffer))
-
-;; (with-eval-after-load 'awk-mode
-;;   (define-key awk-mode-map
-;;     (kbd "C-c C-c")
-;;     #'my-eval-buffer))
-
-
-;; (add-hook 'awk-mode-hook
-;;           (lambda ()
-;;             (local-set-key
-;;              (kbd "C-c C-c")
-;;              #'my-eval-buffer)))
-
-
-
-(defvar my--build-command-hooks
-  '(c-mode-hook
-    c++-mode-hook
-    python-mode-hook
-    js-mode-hook
-    sh-mode-hook
-    awk-mode-hook))
-
-(mapc
- (lambda (hook)
-   (add-hook hook
-             (lambda ()
-               (local-set-key
-                (kbd "C-c C-c")
-                #'my-eval-buffer))))
- my--build-command-hooks)
-
+;; `awk-mode` keybinding not working
+(add-hook 'awk-mode-hook
+          (lambda ()
+            (local-set-key
+             (kbd "C-c C-c")
+             #'my-eval-buffer)))
